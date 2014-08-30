1fc3c582f46d36b6546136ca7aea0aca0f37b3b9
Geoff Pavey <geoff.pavey@gmail.com>
2014-08-30 15:46:01 -0700

    still working on services

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,5 +7,54 @@ $(document).ready(function(){
     offset:{top: 125}
   });
 
+  $('#btn0').click(function(){
+    $('.services_intro').show();
+    $('#services_personal_info').hide();
+    $('#services_choices').hide();
+    $('#services_bike_info').hide();
+  });
+
+  $('#btn1').click(function(){
+    $('#services_intro').hide();
+    $('#services_personal_info').show();
+    $('#services_choices').hide();
+    $('#services_bike_info').hide();
+  });
+
+  $('#btn1b').click(function(){
+    $('#services_intro').hide();
+    $('#services_personal_info').show();
+    $('#services_choices').hide();
+    $('#services_bike_info').hide();
+  });
+
+  $('#btn2').click(function(){
+    $('#services_intro').hide();
+    $('#services_personal_info').hide();
+    $('#services_choices').show();
+    $('#services_bike_info').hide();
+  });
+
+  $('#btn2b').click(function(){
+    $('#services_intro').hide();
+    $('#services_personal_info').hide();
+    $('#services_choices').show();
+    $('#services_bike_info').hide();
+  });
+
+    $('#btn3').click(function(){
+    $('#services_intro').hide();
+    $('#services_personal_info').hide();
+    $('#services_choices').hide();
+    $('#services_bike_info').show();
+  });
+
+    $('#reset').click(function(){
+    $('#services_intro').hide();
+    $('#services_personal_info').show();
+    $('#services_choices').hide();
+    $('#services_bike_info').hide();
+  });
+
 });
 
