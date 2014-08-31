4d715879430014f25b2ea9dca6f3ceeaa9dc1af7
Geoff Pavey <geoff.pavey@gmail.com>
2014-08-30 19:37:01 -0700

    still working on services

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,60 +1,60 @@
 'use strict';
 $(document).ready(function(){
 
-  $(window).stellar({horizontalScrolling:false});
+    $(window).stellar({horizontalScrolling:false});
 
-  $('#my-affix').affix({
-    offset:{top: 125}
-  });
+    $('#my-affix').affix({
+        offset:{top: 125}
+    });
 
-  $('#btn0').click(function(){
-    $('.services_intro').show();
-    $('#services_personal_info').hide();
-    $('#services_choices').hide();
-    $('#services_bike_info').hide();
-  });
+    $('#btn0').click(function(){
+        $('.services_intro').show();
+        $('#services_personal_info').hide();
+        $('#services_choices').hide();
+        $('#services_bike_info').hide();
+    });
 
-  $('#btn1').click(function(){
-    $('#services_intro').hide();
-    $('#services_personal_info').show();
-    $('#services_choices').hide();
-    $('#services_bike_info').hide();
-  });
+    $('#btn1').click(function(){
+        $('#services_intro').hide();
+        $('#services_info').show();
+        $('#services_choices').hide();
+        $('#services_bike_info').hide();
+    });
 
-  $('#btn1b').click(function(){
-    $('#services_intro').hide();
-    $('#services_personal_info').show();
-    $('#services_choices').hide();
-    $('#services_bike_info').hide();
-  });
+    $('#btn1b').click(function(){
+        $('#services_intro').hide();
+        $('#services_info').show();
+        $('#services_choices').hide();
+        $('#services_bike_info').hide();
+    });
 
-  $('#btn2').click(function(){
-    $('#services_intro').hide();
-    $('#services_personal_info').hide();
-    $('#services_choices').show();
-    $('#services_bike_info').hide();
-  });
+    $('#btn2').click(function(){
+        $('#services_intro').hide();
+        $('#services_info').hide();
+        $('#services_choices').show();
+        $('#services_bike_info').hide();
+    });
 
-  $('#btn2b').click(function(){
-    $('#services_intro').hide();
-    $('#services_personal_info').hide();
-    $('#services_choices').show();
-    $('#services_bike_info').hide();
-  });
+    $('#btn2b').click(function(){
+        $('#services_intro').hide();
+        $('#services_info').hide();
+        $('#services_choices').show();
+        $('#services_bike_info').hide();
+    });
 
     $('#btn3').click(function(){
-    $('#services_intro').hide();
-    $('#services_personal_info').hide();
-    $('#services_choices').hide();
-    $('#services_bike_info').show();
-  });
+        $('#services_intro').hide();
+        $('#services_info').hide();
+        $('#services_choices').hide();
+        $('#services_bike_info').show();
+    });
 
     $('#reset').click(function(){
-    $('#services_intro').hide();
-    $('#services_personal_info').show();
-    $('#services_choices').hide();
-    $('#services_bike_info').hide();
-  });
+        $('#services_intro').hide();
+        $('#services_info').show();
+        $('#services_choices').hide();
+        $('#services_bike_info').hide();
+    });
 
 });
 
